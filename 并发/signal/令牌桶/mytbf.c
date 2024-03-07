ca68d10715ae97de7ec2aac3e54aa7e84b674e07
lemonsoda-rpg <1178010161@qq.com>
2024-03-07 20:06:10 +0800

    20240307

---
+++ 并发/signal/令牌桶/mytbf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mytbf.h"
+
+struct mytbf_st
+{
+    int cps;
+    int token;
+    int burst;
+}
---
+++ 并发/signal/令牌桶/mytbf.h
@@ -0,0 +1,15 @@
+#ifndef MYTBF_H_
+#define MYTBF_H_
+
+#define MYTBF_MAX 1024  //桶的最大值
+typedef void mytbf_st 
+mytbf_st *mytbf_init(int cps,int burst);
+
+
+
+
+
+
+
+
+#endif
