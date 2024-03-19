14cc3db6ee72cc7e80c7211b60398a6214fb9b0a
lemonsoda-rpg <1178010161@qq.com>
2024-03-19 14:29:06 +0800

    20240319 14.29

--- advancedIO/epoll.c
+++ advancedIO/epoll.c
@@ -1,13 +1,3 @@
-#include <sys/epoll.h>
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-    epoll_create
-    epoll_ctl
-    epoll_wait
-}
-
 /*
 这是一个忙等的程序
 体现在如果我们一直读不到内容 就会一直返回-1
@@ -22,7 +12,7 @@ int main()
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/select.h>
+#include <sys/epoll.h>
 
 #define TTY1 "/dev/tty11"
 #define TTY2 "/dev/tty12"
@@ -123,14 +113,14 @@ static int max(int fd1,int fd2)
 }
 void relay(int fd1,int fd2)
 {
-    struct pollfd fdlist[2];
+    
     struct fsm_st fsm12,fsm21;
-
+    struct epoll_event ev;
     memset(fsm12.buffer, 0, sizeof(fsm12.buffer));
     
     memset(fsm21.buffer, 0, sizeof(fsm21.buffer));
 
-    fd_set rset,wset;
+   
 
     int fd1_save = fcntl(fd1,F_GETFL);
     int fd2_save = fcntl(fd2,F_GETFL);
@@ -145,39 +135,53 @@ void relay(int fd1,int fd2)
     fsm21.sfd = fd2;
     // printf("断点2\n");
 
-    // 添加文件描述符
-    fdlist[0].fd = fd1;
-    fdlist[1].fd = fd2;
-   
+    int epofd = epoll_create(10);
+    if(epofd<0)
+    {
+        perror("epo_create");
+        exit(1);
+    }
+    ev.events = 0;
+    ev.data.fd = fd1;
+    epoll_ctl(epofd,EPOLL_CTL_ADD,fd1,&ev);
+    
+    ev.events = 0;
+    ev.data.fd = fd2;
+    epoll_ctl(epofd,EPOLL_CTL_ADD,fd2,&ev);
     while(fsm12.state != STATE_T || fsm21.state != STATE_T)
     {
         // 布置监视任务
-        fdlist[0].events = 0;
-        fdlist[1].events = 0;
-        
+        ev.events = 0;
+        ev.data.fd = fd1;
         if(fsm12.state==STATE_R)
         {   
-           fdlist[0].events |=  POLLIN;
-        }
-        if(fsm12.state==STATE_W)
-        {
-            fdlist[1].events |=  POLLOUT;
-        }
-        if(fsm21.state==STATE_R)
-        {
-            fdlist[1].events |=  POLLIN;
+           ev.events |= EPOLLIN;
         }
         if(fsm21.state==STATE_W)
         {
-            fdlist[0].events |=  POLLOUT;
+           ev.events |= EPOLLOUT;
         }
+        epoll_ctl(epofd,EPOLL_CTL_MOD,fd1,&ev);
+        
+        ev.events = 0;
+        ev.data.fd = fd2;
+
+        if(fsm12.state==STATE_W)
+        {
+            ev.events |= EPOLLOUT;       
+        }
+        if(fsm21.state==STATE_R)
+        {
+            ev.events |= EPOLLIN;
+        }
+        epoll_ctl(epofd,EPOLL_CTL_MOD,fd2,&ev);
 
         // 监视
         if(fsm12.state < STATE_AUTO ||fsm21.state<STATE_AUTO)
         {   
 
 
-            while(poll(fdlist,2,-1)<0)
+            while(epoll_wait(epofd,&ev,1,-1)<0)  //1表示只要发生了一个事件那就返回
             {
                 if(errno == EINTR)
                 {
@@ -188,17 +192,18 @@ void relay(int fd1,int fd2)
             }
 
         // 查看监视结果
-            if(fdlist[0].revents&&POLLIN||fdlist[1].revents&&POLLOUT)
+            if(ev.data.fd==fd1&&ev.events&&EPOLLIN ||ev.data.fd==fd2&&ev.events&&EPOLLOUT)
                 fsm_drive(&fsm12);
-            if(fdlist[1].revents&&POLLIN||fdlist[0].revents&&POLLOUT)
+            
+            if(ev.data.fd==fd2&&ev.events&&EPOLLIN ||ev.data.fd==fd1&&ev.events&&EPOLLOUT)
                 fsm_drive(&fsm21);
 
         }
     // printf("断点3\n");
-
+    }  
+    close(epofd);
     fcntl(fd1,F_SETFL,fd1_save);
     fcntl(fd2,F_SETFL,fd2_save);
-    }
 }
 
 
---
+++ advancedIO/mmp/mmp.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+
+int main()
+{
+
+    
+
+
+
+
+    mmp();
+    munmap();
+}
